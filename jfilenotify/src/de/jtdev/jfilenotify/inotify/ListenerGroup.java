c671bee5ebade313110a43eaf216238ea2a3e5f6
tobain <tobain@a8929b34-c232-0410-8221-e6882ed33280>
2007-06-13 23:23:29 +0000

    Added some methods

--- jfilenotify/src/de/jtdev/jfilenotify/inotify/ListenerGroup.java
+++ jfilenotify/src/de/jtdev/jfilenotify/inotify/ListenerGroup.java
@@ -12,17 +12,60 @@ import java.util.LinkedList;
  * 
  * @author Tobias Oelgarte
  */
-public class ListenerGroup {
+public class ListenerGroup implements Comparable<ListenerGroup> {
 	
 	private int watchDescriptor;
 	private LinkedList<FileNotifyListener> listenerList = new LinkedList<FileNotifyListener>();
 	private int combinedMask = 0x00000000;
 	private String lastFileName = null;
 	
+	/**
+	 * Creates a new ListenerGroup with its unique watch descriptor.
+	 * 
+	 * @param watchDescriptor
+	 *        the unique watch descriptor of this group
+	 */
 	public ListenerGroup(int watchDescriptor) {
 		this.watchDescriptor = watchDescriptor;
 	}
 	
+	/**
+	 * Returns the unique watch descriptor used by this group.
+	 * 
+	 * @return the unique watch descriptor
+	 */
+	public int getWatchDescriptor() {
+		return watchDescriptor;
+	}
 	
+	/**
+	 * Adds the listener to this group and updates the most common mask for 
+	 * inotify.
+	 * 
+	 * @param listener
+	 *        the listener that will be added to this group
+	 */
+	public void addListener(FileNotifyListener listener) {
+		synchronized (listenerList) {
+			// make sure that the same listener can only be once registered
+			if (!listenerList.contains(listener))
+				listenerList.add(listener);
+		}
+		// TODO Option ONLY_DIRECTORY must be cleared if only one listener is 
+		// listening in not only directory mode.
+		combinedMask |= listener.getMask();
+		lastFileName = listener.getFileName();
+	}
+
+	/**
+	 * Compares to groups by the number of their watch descriptor.
+	 * 
+	 * @param g
+	 *        the group to compare with
+	 * @return the arithmetic diffrence between the  watch descriptors
+	 */
+    public int compareTo(ListenerGroup g) {
+		return this.getWatchDescriptor() - g.getWatchDescriptor();
+    }
 	
 }
