d2ad8598be45ea2cf3687b6803acbd112fe10390
tobain <tobain@a8929b34-c232-0410-8221-e6882ed33280>
2007-06-13 09:42:11 +0000

    initial import

---
+++ jfilenotify/src/de/jtdev/jfilenotify/FileNotifyConstants.java
@@ -0,0 +1,27 @@
+package de.jtdev.jfilenotify;
+
+/**
+ * This constants are currently very close related to inotify, but not all
+ * implementations can provide such fine notification.
+ */
+public interface FileNotifyConstants {
+
+	public static final int OPENED               = 0x00000001;
+	public static final int ACCESSED             = 0x00000002;
+	public static final int MODIFIED             = 0x00000004;
+	public static final int CLOSED_WRITEABLE     = 0x00000008;
+	public static final int CLOSED_NOT_WRITEABLE = 0x00000010;
+	public static final int ATTRIBUTES_CHANGED   = 0x00000020;
+
+	public static final int SUBFILE_CREATED      = 0x00000100;
+	public static final int SUBFILE_DELETED      = 0x00000200;
+	public static final int MOVED_FROM           = 0x00000400;
+	public static final int MOVED_TO             = 0x00000800;
+	public static final int SELF_DELETED         = 0x00001000;
+	public static final int SELF_MOVED           = 0x00002000;
+	public static final int UNMOUNTED            = 0x00004000;
+
+	public static final int ONLY_DIRECTORY       = 0x00010000;
+	public static final int DONT_FOLLOW          = 0x00020000;
+
+}
