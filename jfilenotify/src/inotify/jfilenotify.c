a3c641ce8dba2430609a4e48e3fe6cb404a37d7b
tobain <tobain@a8929b34-c232-0410-8221-e6882ed33280>
2007-06-14 14:25:11 +0000

    code for reading events partialy added

--- jfilenotify/src/inotify/jfilenotify.c
+++ jfilenotify/src/inotify/jfilenotify.c
@@ -33,4 +33,32 @@ JNIEXPORT jint JNICALL Java_de_jtdev_jfilenotify_inotify_INotifyService_removeWa
 	int ret = inotify_rm_watch(fd, wd);
 	if (ret < 0) return (jint) -errno;
 	return (jint) ret;
-}	
+}
+
+#define EVENT_SIZE (sizeof(struct inotify_event))
+#define BUF_LEN (1024 * (EVENT_SIZE + 16))
+
+char buf[BUF_LEN];
+
+JNIEXPORT jobjectArray JNICALL Java_de_jtdev_jfilenotify_inotify_INotifyService_readEvents
+(JNIEnv *env, jobject caller, jint fd) {
+	int len, i = 0;
+	len = read (fd, buf, BUF_LEN);
+	if (len < 0) {
+		if (errno = EINTR)
+			;// need to reissue system call
+		else
+			;// return null (error)
+	} else if (len == 0) {
+		// BUF_LEN too small?
+	}
+	
+	while (i < len) {
+		struct inotify_event *event;
+		event = (struct inotify_event *) &buf[i];
+		if (event->len > 0) {
+			// filename
+		}
+		i += EVENT_SIZE + event->len;
+	}
+}
